0b2f3e6bbd8eea2e0df76795708dc9f23d7aa5bd
Vintagejojo <jdnal7@gmail.com>
2021-05-13 09:49:28 -0500

    styled styled the cart item component and productscreen component

---
+++ frontend/src/components/CartItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CartItem.css'
+
+const CartItem = () => {
+    return (
+        <div className="cartitem">
+            CartItems
+        </div>
+    )
+}
+
+export default CartItem
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import CartItem from '../components/CartItem';
 import './CartScreen.css';
 
 export const CartScreen = () => {
     return (
         <div className="cartscreen">
-            CartScreen
+            <div className="cartdisplay_left">
+                <h2>Shopping Cart</h2>
+                <CartItem />
+            </div>
+            <div className="cartdisplay_right">
+                <div className="cartdisplay_deets">
+                    <p>Subtotal (0) items </p>
+                    <p>$999.99 </p>
+                </div>
+                <div>
+                    <button>Proceed To Checkout </button>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
